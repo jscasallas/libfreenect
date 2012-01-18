2e3e892f89ddfaa8dd4f885d9d9bd1ae75688407
Juan Sebastian Casallas <casallas@iastate.edu>
2012-01-17 22:22:12 -0600

    libfreenect.rb: depend on official libusb formula
    -libusb's HEAD no longer needs patches to work with libfreenect
    -Added caveats to explain the need to install libusb's HEAD
    -Added test, that executes the glview example
    -url now points to latest tag, 1.1, and head to the git repo

--- platform/osx/homebrew/libfreenect.rb
+++ platform/osx/homebrew/libfreenect.rb
@@ -1,13 +1,13 @@
 require 'formula'
 
-class Libfreenect <Formula
-  url 'https://github.com/OpenKinect/libfreenect/tarball/master'
-  version 'master'
+class Libfreenect < Formula
+  head 'https://github.com/OpenKinect/libfreenect.git'
+  url 'https://github.com/OpenKinect/libfreenect/tarball/v0.1.1'
+  md5 'bf874e29274171a89520c80de9795cbd'
   homepage 'http://openkinect.org'
-  md5 ''
-  
-  depends_on 'libusb-freenect'
-  depends_on 'cmake'
+
+  depends_on 'libusb' #head
+  depends_on 'cmake' => :build
 
   def install
     mkdir "build"
@@ -15,4 +15,16 @@ class Libfreenect <Formula
     system "cmake .. #{std_cmake_parameters}"
     system "make install"
   end
+
+  def test
+    system "glview"
+  end
+
+  def caveats; <<-EOS.undent
+    Due to some bugs in libusb's release, this formula
+    requires libusb's HEAD, to install it do:
+    `brew install libusb --HEAD`
+    EOS
+  end
+
 end
